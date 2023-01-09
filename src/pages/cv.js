57de0c27f5c9053ea7a2686e397d67048d1347d2
Omar Abdelghany <oabdelghany1409@gmail.com>
2023-01-10 02:29:56 +0300

    added cv to page

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,16 +1,19 @@
 import React from "react";
-import latestCv from './latestCv.pdf'
+import finalCv from './finalCv.pdf'
+import { Document,Page,pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 const Cv =()=>{
     return(
         <div className="resume">
         <h1>Below you can find a copy of my cv</h1>
         <div className="resume-pdf">
-        <embed 
-        src={latestCv + "#toolbar=0"}
-        type="application/pdf"
-        height={800}
-        width={900}
+        <Document file={finalCv}
+        onContextMenu={(e) => e.preventDefault()}>
+        <Page pageNumber={1}
+        renderTextLayer={false}
+        renderAnnotationLayer={false}
         />
+        </Document>
         </div>
         </div>
     )
