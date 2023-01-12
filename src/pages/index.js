7e54aec09beef9c8a9b38b67c049dd0b029db1d2
Omar Abdelghany <oabdelghany1409@gmail.com>
2023-01-12 03:21:26 +0300

    finished Homepage styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,66 +8,72 @@ import '../Styles/home.css';
 const Home = () => {
   return (
     <div className="home">
-      <div className="introduction">
-      <h2>Hi, My Name is Omar</h2>
-      <div className='phrase'>
-        <p className='description'>A Software Engineering student at the University of Ottawa with a passion for learning and creating</p>
-      </div>
-      </div>
-  
-        <h1 className="title">SkillSet</h1 >
-        <Grid id="skills" container alignItems="center" justifyContent="center">
-          <Grid className='skillitem'>
-          <span className="fa-stack fa-2x">
-             <i className="fa-brands fa-python fa-stack-1x" style={{zIndex: "1"}}></i>
-              <i id= "pycircle" className="fa-solid fa-circle  fa-stack-2x"></i>
-          </span> 
-          <p className='name'>Python</p>
+    <div className="introduction">
+    <h2>Hi, My Name is Omar</h2>
+    <div className='phrase'>
+      <p className='description'>A Software Engineering student at the University of Ottawa with a passion for learning and creating</p>
+    </div>
+    </div>
 
-          </Grid >
-          <Grid className='skillitem'>
-          <span className="fa-stack fa-2x">
-             <i className="fa-brands fa-java fa-stack-1x"  style={{zIndex: "1"}}></i>
-              <i  id= "javacircle"className="fa-solid fa-circle  fa-stack-2x"></i>
-          </span> 
-          <p className='name'>Java</p>
-
-          </Grid >
-          <Grid className='skillitem'>
-          <span className="fa-stack fa-2x">
-             <i className="fa-brands fa-html5 fa-stack-1x"  style={{zIndex: "1"}}></i>
-              <i id ="htmlcircle"className="fa-solid fa-circle  fa-stack-2x"></i>
-          </span> 
-          <p className='name'>HTML</p>
-          </Grid >
-          <Grid className='skillitem'>
-          <span className="fa-stack fa-2x">
-             <i className="fa-brands fa-css3 fa-stack-1x"  style={{zIndex: "1"}}></i>
-              <i id ="csscircle"className="fa-solid fa-circle  fa-stack-2x"></i>
-          </span> 
-          <p className='name'>CSS</p>
-
-          </Grid >
-
-          <Grid className='skillitem'>
-          <span className="fa-stack fa-2x">
-             <i className="fa-brands fa-js fa-stack-1x" style={{zIndex: "1"}} ></i>
-              <i id ="jscircle"className="fa-solid fa-circle  fa-stack-2x"></i>
-          </span> 
-          <p className='name'>JavaScript</p>
-          </Grid >
-
-          
-          <Grid className='skillitem'>
-          <span className="fa-stack fa-2x">
-             <i className="fa-brands fa-react fa-stack-1x"  style={{zIndex: "1"}}></i>
-              <i id ="reactcircle" className="fa-solid fa-circle fa-stack-2x"></i>
-          </span> 
-          <p className='name'>React</p>
-          </Grid >
+      <h1 className="title">SkillSet</h1 >
+    <div style={{padding: "4% 0%"}}>
+     <Grid id="Skills" container alignItems="center" justifyContent="center">
+        <Grid className="skillItem">
+            <span className="fa-stack fa-2x" >
+                <i className="fa-brands fa-python fa-stack-1x" style={{zIndex: "1"}}></i>
+                <i id="pycircle"className="fa-solid fa-circle fa-stack-2x " ></i>
+            </span>
+            <p>Python</p>
+         
         </Grid>
-      </div>
-  );
-};
+        <Grid className="skillItem">
+            <span className="fa-stack fa-2x">
+                <i class="fa-brands fa-java fa-stack-1x" style={{zIndex: "1"}}></i>
+                <i id="javacircle"className="fa-solid fa-circle fa-stack-2x"></i>
+            </span>
+            <p>Java</p>
+           
+        </Grid>
+        <Grid className="skillItem">
+            <span className="fa-stack fa-2x">
+                <i className="fa-brands fa-html5 " style={{zIndex: "1"}}></i>
+                <i id="htmlcircle" className="fa-solid fa-circle fa-stack-2x"></i>
+            </span>
+            <p>HTML</p>
+            
+        </Grid>
+        <Grid className="skillItem">
+            <span className="fa-stack fa-2x">
+                <i class="fa-brands fa-css3 fa-stack-1x " style={{zIndex: "1"}}></i>
+                <i id ="csscircle"className="fa-solid fa-circle fa-stack-2x"></i>
+            </span>
+            <p>CSS</p>
+           
+        </Grid>
+        <Grid className="skillItem">
+            <span className="fa-stack fa-2x">
+                <i className="fa-brands fa-js fa-stack-1x " style={{zIndex: "1"}}></i>
+                <i id="jscircle" className="fa-solid fa-circle fa-stack-2x"></i>
+            </span>
+            <p>JavaScript</p>
+            
+        </Grid>
+        <Grid className="skillItem">
+            <span className="fa-stack fa-2x">
+                <i class="fa-brands fa-react fa-stack-1x" style={{zIndex: "1"}}></i>
+                <i id="reactcircle"className="fa-solid fa-circle fa-stack-2x"></i>
+            </span>
+            <p>React</p>
+          
+        </Grid>
+    
+       
+     </Grid>
+    </div>
+    </div>
+    
+ );
+}
+
   
 export default Home;
