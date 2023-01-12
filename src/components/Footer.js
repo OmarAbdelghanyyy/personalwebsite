2c2ef2d778c57ac6aa64f1095c58e8bdad5ae580
Omar Abdelghany <oabdelghany1409@gmail.com>
2023-01-12 03:33:32 +0300

    added footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,20 @@
 import React from "react";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Link from '@mui/material/Link'
 import GitHubIcon from '@mui/icons-material/GitHub';
 import '../Styles/Footer.css'
+import { ClassNames } from "@emotion/react";
 const Footer= ()=>{
     return(
     <div className="footer">
     <div className="media">
-        <LinkedInIcon style={{color:"white"}} />
-        <GitHubIcon style={{color:"white"}} />  
-        <p>@
-            Copyright 
-            2023
+        <Link href="https://www.linkedin.com/in/omar-abdelghany-027585254/" className={ClassNames.Link} target="_blank">
+            <LinkedInIcon style={{color:"white"}}  href="https://www.linkedin.com/in/omar-abdelghany-027585254/"/>
+        </Link>
+        <Link href="https://github.com/OmarAbdelghanyyy" className={ClassNames.Link} target ="_blank">
+             <GitHubIcon style={{color:"white"}} />  
+        </Link>
+        <p>   @Copyright 2023
         </p>
 
     </div>
